6cf8eb4c7035ba7695bc5da298d2fc64ecb753ec
lobna3 <yousfi55lobna@gmail.com>
2023-06-04 16:27:13 +0100

    initial project

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   Route
 } from "react-router-dom";
 import Home from './pages/Home';
-import Details from './pages/Detail';
+import Details from './pages/Details';
 
 function App() {
   return (
