62ebbb77db5910507144d340517a299b629e5493
lobna3 <yousfi55lobna@gmail.com>
2023-06-04 13:56:20 +0100

    initial project

--- controllers/clients.js
+++ controllers/clients.js
@@ -33,13 +33,10 @@ const getClientById = async(req, res) =>{
     }
 };
 
-
-
 const postClients =async(req, res) => {
     try {
         const client = await Client.create(req.body)
         res.status(200).json(client);
-        
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
